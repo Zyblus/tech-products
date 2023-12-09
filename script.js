29a1fc1c2b746b416cd1d1a2ebd9a3f5a6f16a0e
Zyblus-tech <126899754+Zyblus@users.noreply.github.com>
2023-12-09 11:46:57 +0545

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function myfun() {
     y = document.getElementById('phones');
 
     if (a == "") {
-        alert("Search-bar cannot be empty !!!")
+        alert("Search-bar cannot be empty !!!");
     }
 
     if (a === "laptop" || a === "laptops" || a === "Laptops" || a === "LAPTOPS" || a === "LAPTOP") {
@@ -16,9 +16,47 @@ function myfun() {
         window.location.href = 'index2.html';
     }
 
+    else if (a === "CALCULATOR") {
+        window.location.href = 'calcul.html';
+    }
+
     else if (a != "") {
         alert("Items not found.")
     }
 }
 
 
+function myfun1() {
+
+    a = document.getElementById('extract').value;
+
+    if (event.key === 'Enter' && a == "") {
+        alert("Search-bar cannot be empty !!!");
+    }
+
+    if (event.key === "Enter" && (a === "laptop" || a === "laptops" || a === "Laptops" || a === "LAPTOPS" || a === "LAPTOP")) {
+        window.location.href = 'index1.html';
+    }
+
+    else if (event.key === "Enter" && (a === "mobile" || a === "mobiles" || a === "phones" || a === "phone" || a === "Phone" || a === " PHONES" || a === "Mobile" || a === "Mobiles")) {
+        window.location.href = 'index2.html';
+    }
+
+    else if (event.key === "Enter" && (a === "CALCULATOR" || a === "calculator" || a === "Calculator")) {
+        window.location.href = 'calculator.html';
+    }
+
+    else if (event.key === "Enter" && a != '') {
+        alert("Items not found.");
+    }
+
+
+    // Check if the pressed key is the slash key ("/") and the search input is not already focused
+    if (event.key === '/' && document.activeElement !== a) {
+        // Focus on the search input
+        a.focus();
+
+        // Prevent the default behavior of the slash key (e.g., inserting the '/' character in the input)
+        event.preventDefault();
+    }
+}
